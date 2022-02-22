ad7406111f3c97c30982ade245bac3f3ea6a64be
Joseph Maramba <josephmaramba50@gmail.com>
2022-02-22 19:28:09 +0300

    reconfiguration with lerna

--- web/src/components/DarkMode/DarkMode.jsx
+++ web/src/components/DarkMode/DarkMode.jsx
@@ -1,8 +1,37 @@
 import * as React from "react";
+import "./DarkMode.scss";
+import { motion, AnimatePresence } from "framer-motion";
+import { FaMoon } from "@react-icons/all-files/fa/FaMoon";
+import { FaSun } from "@react-icons/all-files/fa/FaSun";
 
 const DarkMode = () => {
-  const [isDark, setisDark] = useState(false);
-  return <div>DarkMode is me</div>;
+  const [isDark, setisDark] = React.useState(false);
+  const switchStyles = {
+    justifyContent: isDark ? "flex-end" : "flex-start",
+  };
+
+  const handleClick = () => {
+    setisDark(!isDark);
+  };
+
+  return (
+    <div
+      className="container"
+      style={switchStyles}
+      data-mode={isDark}
+      onClick={handleClick}
+    >
+      <AnimatePresence>
+        <motion.div layout className="switch">
+          {isDark ? (
+            <FaMoon className="switch__moon" />
+          ) : (
+            <FaSun className="switch__sun" />
+          )}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default DarkMode;
