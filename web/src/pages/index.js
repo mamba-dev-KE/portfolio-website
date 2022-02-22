9272b61bc30e8929d3179e5636a6bf78d783ba9f
Joseph Maramba <josephmaramba50@gmail.com>
2022-02-22 20:51:46 +0300

    fixing build error

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -4,10 +4,7 @@ import "normalize.css";
 import NavBar from "../components/NavBar/NavBar";
 import Layout from "../components/Layout/Layout";
 
-
 export default function Home({ data }) {
-  const { edges } = data?.allSanityBlogPost;
-
   return (
     <>
       <React.StrictMode>
@@ -17,21 +14,3 @@ export default function Home({ data }) {
     </>
   );
 }
-
-export const query = graphql`
-  query MyQuery {
-    allSanityBlogPost {
-      totalCount
-      edges {
-        node {
-          _id
-          name
-          sponsor {
-            name
-            url
-          }
-        }
-      }
-    }
-  }
-`;
